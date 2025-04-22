96505d87ddb75ec85ec0154a7d4c1bbf18a2b236
unknown <modaralshibani@gmail.com>
2025-04-22 14:02:42 +0300

    hi

---
+++ lib/core/theming/text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Title Text Style
+TextStyle titleTextStyle = const TextStyle(
+  fontSize: 24, // Example: Increased for titles
+  fontWeight: FontWeight.bold, // Corrected: Access static member directly
+  color: Colors.black87, // Example: Darker color for better contrast
+);
+
